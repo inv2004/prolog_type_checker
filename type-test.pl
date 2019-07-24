066cbfb0780fdc6f0b47b585083dd894c4801abd
inv2004 <inv2004@gmail.com>
2019-07-23 22:50:43 -0400

    ext returns list

--- type-test.pl
+++ type-test.pl
@@ -12,18 +12,17 @@ ext_list([], _) :- false.
 ext_list([L|LL], Y) :-
     ext(L, Y);
     ext_list(LL, Y).
-ext([X], Y) :- t([X],_),X=Y.
+ext([X], Y) :- t([X],_),[X]=Y.
 ext([X|XX], Y) :-
     t([X|_],_),Y=[X|Z],ext(XX,Z);
     t([X|XX], L),ext_list(L, Y).
 
 eval([], D, Y) :- D=Y.
-eval([P|PP], [D|DD], Y) :- ext([P],A),A=D,eval(PP, DD, Y).
+eval([P|PP], [D|DD], Y) :- ext([P],A),A=[D],eval(PP, DD, Y).
 
 main(X) :-
-    E = _{t:sum, p:[num3, num3] , w:_{sum: [num3, num1, num3]}},
+    E = _{t:sum, p:[num3, num3] , w:_{sum: [num3, num3, num3]}},
     T = E.get(t),
     P = E.get(p),
     D = E.get(w).get(T),
     eval(P, D, X).
-
